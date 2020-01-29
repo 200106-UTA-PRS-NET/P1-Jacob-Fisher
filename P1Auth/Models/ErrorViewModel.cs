343408883a893a5edc5e84e2af12df8f0ed8d309
Jacob Fisher <jacobdf@cs.unc.edu>
2020-01-29 16:53:29 -0600

    test

---
+++ P1Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace P1Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
