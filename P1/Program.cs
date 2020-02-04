a2e0ebcec2f38a8d1612d5fcb166fdf724e91015
Jacob Fisher <jacobdf@cs.unc.edu>
2020-02-04 13:12:19 -0600

    Added Logging to Main

--- P1/Program.cs
+++ P1/Program.cs
@@ -13,27 +13,29 @@ namespace P1
     {
         public static void Main(string[] args)
         {
-            //var config = new NLog.Config.LoggingConfiguration();
+            var config = new NLog.Config.LoggingConfiguration();
 
-            //// Targets where to log to: File and Console
-            //var logfile = new NLog.Targets.FileTarget("logfile") { FileName = $"file{DateTime.Now.ToString()}.txt" };
-            ////var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = $"file{DateTime.Now.ToString()}.txt" };
+            //var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
 
-            //// Rules for mapping loggers to targets
-            //config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            // Rules for mapping loggers to targets
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
 
-            //// Apply config           
-            //NLog.LogManager.Configuration = config;
+            // Apply config           
+            NLog.LogManager.Configuration = config;
 
-            //NLog.Logger _log_ = NLog.LogManager.GetCurrentClassLogger();
+            NLog.Logger _log_ = NLog.LogManager.GetCurrentClassLogger();
 
-            //try { 
-            CreateHostBuilder(args).Build().Run();
-            //} catch (Exception e)
-            //{
-            //    //_log_.Fatal(e.Message);
-            //    //_log_.Fatal(e.StackTrace);
-            //}
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                _log_.Fatal(e.Message);
+                _log_.Fatal(e.StackTrace);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
